66738ce9779b7579a04c96beddff9c8e17c6fae3
Your Name <tfikrie@yahoo.com>
2019-08-09 22:19:11 -0400

    delete and update for the posts completed

--- db-router.js
+++ db-router.js
@@ -32,11 +32,43 @@ router.get('/:id',async(req, res)=>{
     try {
     const id = req.params.id
     const post = await db.findById(id)
-    if(!post){res.status(404).json({ message: "The post with the specified ID does not exist." })}
-    res.status(200).json(post)
+    if(post[0]){ res.status(200).json(post)}else {
+        {res.status(404).json({ message: "The post with the specified ID does not exist." })}
+    }
     } catch(error){
         console.log(error)
         res.status(500).json({error: "The post information could not be retrieved."})
+    }
+})
+
+router.delete('/:id',async(req, res)=>{
+    try{
+        const id = req.params.id
+        const deleted = await db.remove(id)
+        if(deleted){res.status(204).json({message:`id ${id} is successfully deleted`})}else {
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+    } catch(error){
+        console.log(error)
+        res.status(500).json({ error: "The post could not be removed" })
+    }
+})
+
+router.put('/:id', async(req, res)=>{
+    const updatepost = req.body
+    const id = req.params.id
+    const {title, contents} = updatepost
+    if(!title || !contents){
+        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+    }
+    try {
+        const count = await db.update(id, updatepost)
+        if(count){res.status(201).json(updatepost)}else{
+            res.status(404).json({ message: "The post with the specified ID does not exist." })
+        }
+        
+    } catch(error){
+        res.status(500).json({  error: "The post information could not be modified."})
     }
 })
 
